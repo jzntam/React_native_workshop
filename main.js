df50831d6054c1b66f9b3b1fcb34cbda29a9ac0e
Jason <cameljeet@gmail.com>
2015-10-10 20:25:49 -0700

    Added image to View

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-/**
- * This is the entry point for your experience that you will run on Exponent.
- *
- * Start by looking at the render() method of the component called
- * FirstExperience. This is where the text and example components are.
- */
 'use strict';
 
 var React = require('react-native');
-var { AppRegistry, View, StyleSheet} = React;
+var { AppRegistry, View, StyleSheet, Image} = React;
 
 var MyApp = React.createClass({
   render() {
     return (
-      <View style={styles.base}>
-        <View style={[styles.child, {flex: 1}]} />
-        <View style={[styles.child, {flex: 8, paddingTop: 100, flexDirection: 'row'}]}>
-          <View style={[styles.innerChild, {flex: 2}, styles.green]} />
-          <View style={[styles.innerChild, {flex: 8}, styles.blue]} />
-        </View>
-        <View style={[styles.child, {flex: 1}]} />
+      <View style={[styles.base, {paddingTop: 100}]}>
+        <Image source={{uri: 'http://i.imgur.com/bG99o3V.gif'}}
+          style={{width: 300, height: 300}}
+          resizeMode="cover" />
       </View>
     );
   }
@@ -29,24 +20,6 @@ var styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent:  'center',
-    //
-  },
-  child: {
-    borderWidth: 1,
-    borderColor: 'white',
-    backgroundColor: 'red',
-    flexDirection: 'column',
-  },
-  innerChild: {
-    borderWidth: 2,
-    borderColor: 'gold',
-    height: 200,
-  },
-  blue: {
-    backgroundColor: 'blue',
-  },
-  green: {
-    backgroundColor: 'green',
   },
 });
 
