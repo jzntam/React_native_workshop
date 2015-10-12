b97e24b34450397b94868c96e87231781e13c0bf
Jason <cameljeet@gmail.com>
2015-10-11 21:25:49 -0700

    Interactive. clicker incrementer app

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 'use strict';
 
 var React = require('react-native');
-var { AppRegistry, View, StyleSheet, Image} = React;
+var { AppRegistry, View, StyleSheet, Text} = React;
 
 var MyApp = React.createClass({
+
+  getInitialState() {
+    return {count: 0};
+  },
+
+  incrementCount() {
+    this.setState({count: this.state.count + 1});
+  },
+
   render() {
     return (
-      <View style={[styles.base, {paddingTop: 100}]}>
-        <Image source={{uri: 'http://i.imgur.com/bG99o3V.gif'}}
-          style={{width: 300, height: 300}}
-          resizeMode="cover" />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          My first React Native app
+        </Text>
+        <Text onPress={this.incrementCount} style={styles.counter}>
+          {this.state.count} clicks
+        </Text>
       </View>
     );
-  }
+  },
 });
 
 var styles = StyleSheet.create({
-  base: {
+  container: {
     flex: 1,
     flexDirection: 'row',
+    alignItems: 'flex-start',
     justifyContent:  'center',
+    paddingTop: 200,
+  },
+  welcome: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'gold',
+    textAlign: 'center',
+    height: 50,
+    width: 180,
+  },
+  counter: {
+    borderWidth: 1,
+    borderColor: 'red',
+    backgroundColor: 'orange',
+    textAlign: 'center',
+    height: 50,
+    width: 180,
   },
 });
 
