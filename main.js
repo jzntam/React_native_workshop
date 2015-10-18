b38ba265127a9b0f91c25b69952ad176e8d3e7f4
Jason Tam <jason@wishpond.com>
2015-10-17 23:37:12 -0700

    Simple Starwars API App

--- main.js
+++ main.js
@@ -1,59 +1,126 @@
-/**
- * This is the entry point for your experience that you will run on Exponent.
- *
- * Start by looking at the render() method of the component called
- * FirstExperience. This is where the text and example components are.
- */
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  View,
-  StyleSheet,
-  Text,
-  Image,
-  ActivityIndicatorIOS,
-  ScrollView,
-  Animated
-} = React;
+var _ = require('underscore');
 
-var MyApp = React.createClass({
+var { ActivityIndicatorIOS, Text, Navigator, TouchableHighlight, AppRegistry, ListView, View, StyleSheet } = React;
 
-  getInitialState() {
-    return { entrance: new Animated.Value(0) };
-  },
-
-  componentDidMount(){
-    Animated.spring(this.state.entrance, {toValue: 1}).start();
-  },
-
-  render(){
-    let opacity = this.state.entrance;
-    let rotate = this.state.entrance.interpolate({
-      inputRange: [0,1],
-      outputRange: ['-180deg', '0deg']
-    });
-
+var StarWarsApp = React.createClass({
+  render() {
     return (
-      <View style={styles.container}>
-        <Animated.View style={[styles.box, { opacity, transform: [{rotate}] } ]} />
+      <View style={{flex: 1}}>
+        <View style={{position: 'absolute', top: 0, left: 0, height: 200, width: 200, backgroundColor: 'red'}} />
+        <Navigator
+          renderScene={this._renderScene}
+          initialRoute={{id: 'home'}} />
       </View>
     );
   },
+
+  _renderScene(route, navigator) {
+    if (route.id === 'home') {
+      return <CharacterList navigator={navigator} />
+    } else if (route.id === 'character') {
+      return <CharacterDetail navigator={navigator} person={route.person} />
+    }
+  }
+});
+
+var CharacterDetail = React.createClass({
+
+  render() {
+    let { person } = this.props;
+
+    return (
+      <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center'}}>
+        <Text>{person.name}</Text>
+        <Text>{person.mass}</Text>
+        <Text>{person.eye_color}</Text>
+      </View>
+    )
+  }
+
+});
+
+var CharacterList = React.createClass({
+  getInitialState() {
+    let dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+
+    return { people: dataSource };
+  },
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/people').
+      then((response) => response.json()).
+      then((response) => {
+        this.setState({
+          hasLoaded: true,
+          people: this.state.people.cloneWithRows(response.results)
+        });
+      });
+  },
+
+  render() {
+    if (!this.state.hasLoaded) {
+      return this._renderLoadingView();
+    }
+
+    return (
+      <View style={{paddingTop: 30, flex: 1}}>
+        <ListView
+          renderSeparator={this._renderSeparator}
+          dataSource={this.state.people}
+          renderRow={this._renderRow} />
+      </View>
+    )
+  },
+
+  _renderLoadingView() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ActivityIndicatorIOS />
+      </View>
+    );
+  },
+
+  _renderRow(rowData) {
+    return (
+      <TouchableHighlight style={styles.row} underlayColor="#ccc"
+        onPress={() => { this.props.navigator.push({id: 'character', person: rowData}) }}>
+        <Text>{rowData.name}</Text>
+      </TouchableHighlight>
+    )
+  },
+
+  _renderSeparator() {
+    return (
+      <View style={{height: 1, flex: 1, backgroundColor: 'green'}} />
+    )
+  }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent:  'center',
-    paddingTop: 200,
+    height: 200,
   },
-  box: {
-    width: 250,
-    height: 250,
-    backgroundColor: 'gold'
+  row: {
+    padding: 20,
   },
+  scrollContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  child: {
+    borderWidth: 1,
+    borderColor: 'white',
+    backgroundColor: 'red',
+  },
+  green: {
+    backgroundColor: 'green',
+  }
 });
 
-AppRegistry.registerComponent('main', () => MyApp);
+AppRegistry.registerComponent('main', () => StarWarsApp);
